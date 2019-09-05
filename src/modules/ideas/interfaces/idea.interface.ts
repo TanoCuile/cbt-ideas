cff4c9e5659168c03f7b8b3af712804401a13b64
SergChr <Serhii.Cheredko@travelport.com>
2019-09-05 11:06:04 +0300

    Add 2 fields to Idea interface

--- src/modules/ideas/interfaces/idea.interface.ts
+++ src/modules/ideas/interfaces/idea.interface.ts
@@ -4,6 +4,8 @@ export interface Idea {
   readonly description: string;
   readonly likes: number;
   readonly dislikes: number;
+  readonly users_who_liked: Array<string>;
+  readonly users_who_disliked: Array<string>;
 
   // refers to user who created an idea
   readonly owner: string;
